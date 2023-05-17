bc533ea00a18ba2a3f7bf7ba5d38950358747fad
arsalanKhawar <ak78@njit.edu>
2023-05-16 20:52:39 -0400

    basic navigation list

--- header.php
+++ header.php
@@ -1,1 +1,12 @@
 <!--This page is for the navigation bar on top of every page in the website-->
+<?php
+    include_once 'header.php';
+?>
+
+<nav>
+    <ul>
+    <li><a href =  'home.php' >Home</a></li>
+    <li><a href =  'login.php' >Login</a></li>
+    <li><a href =  'signup.php' >Signup</a></li>
+    </ul>
+</nav>

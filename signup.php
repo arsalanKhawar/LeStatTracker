d6d8c081fdb87be171f8a7cfb2f8f9d312de4b46
arsalanKhawar <ak78@njit.edu>
2023-05-18 22:51:27 -0400

    added a basic form for signup

--- signup.php
+++ signup.php
@@ -3,3 +3,15 @@
     include_once 'header.php';
 ?>
 <link rel="stylesheet" href="<?php echo 'styles.css'; ?>">
+
+<!-- Creating Registration form -->
+<section class="signup">
+    <h2 id="signuplabel">Sign up</h2>
+    <form action="signup.inc.php" method="post">
+        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="username" placeholder="use">
+        <input type="text" name="password" placeholder="Password">
+        <input type="text" name="confirmpassword" placeholder="Confirm Password">
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</section>

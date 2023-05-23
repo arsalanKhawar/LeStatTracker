12a9af2e8189b3aa4dcbf6ae8e362bd362551678
arsalanKhawar <ak78@njit.edu>
2023-05-23 19:08:59 -0400

    code used for creating user table added

---
+++ sqlcode/createUserTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    userid int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    useremail varchar(128) NOT NULL,
+    username varchar(128) NOT NULL,
+    userpswd varchar(128) NOT NULL
+);

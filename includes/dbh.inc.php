d7fd1638f243728e78c4e82c9de67bc684b30820
arsalanKhawar <ak78@njit.edu>
2023-05-23 18:35:00 -0400

    created database variables

---
+++ includes/dbh.inc.php
@@ -0,0 +1,6 @@
+<!--Initialized database connection. Will be referenced on any page that needs to access the database-->
+<?php
+$dbServerName = "localhost";
+$dbUserName = "root";
+$dbPassword = "";
+$dbName = "lestattracker";

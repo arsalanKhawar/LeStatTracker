3cfb4522d96157aff83357ea04064d8f23e271fc
arsalanKhawar <ak78@njit.edu>
2023-05-23 19:34:57 -0400

    added code to get information from the global variable

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,7 +1,12 @@
 <!--This page will add the new user information into the database. It handles the information sent from signup.php-->
 <?php
     if(isset($_POST["submit"])){
-        echo "hi";
+        $name = $_POST["email"];
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $confirmpassword = $_POST["confirmpassword"];
+        
+        
     }
     else{
         header("location: ../signup.php");

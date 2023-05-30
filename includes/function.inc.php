774abfe8ed74eef69105a2a7714a9c03edc67bd1
arsalanKhawar <ak78@njit.edu>
2023-05-30 17:38:15 -0400

    added usernameExists function

--- includes/function.inc.php
+++ includes/function.inc.php
@@ -15,7 +15,7 @@ function emptySignupInput($email,$username,$password,$confirmpassword){
 // checks if username is valid
 function invalidUsername($username){
     $result = true;
-    if(!preg_match("/^[a-z0-9A-Z]{6,30}$/i", $username)){
+    if(!preg_match("/^[a-z0-9_-]{6,30}$/i", $username)){
         $result = true;
     }
     else{
@@ -24,6 +24,7 @@ function invalidUsername($username){
     return $result;
 }
 
+//checks if user gave a valid email
 function invalidEmail($email){
     $result = true;
     if(!filter_var($email, FILTER_VALIDATE_EMAIL))
@@ -34,4 +35,37 @@ function invalidEmail($email){
         $result = false;
     }
     return $result;
+}
+
+//checks if password matches
+function passwordMatch($password,$confirmpassword){
+    $result = true;
+    if($password !== $confirmpassword){
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+function usernameExists($conn,$username,$email){
+    $sql = "SELECT * from users where username = ? OR useremail = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+        header("location: ../signup.php?error=stmtfailed"); //checks if sql statement works
+            exit();
+    }
+    mysqli_stmt_bind_param($stmt, "ss",$username,$email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+    if($row = mysqli_fetch_assoc($resultData)){ //reutrns data if username or email exists. Otherwise returns false
+        return $row;
+    }
+
+    else{
+        $result = false;
+        return $result;
+    }
 }

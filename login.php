b67fd84b3dfb36c5d67cdf7bd95a3f0a255d247f
arsalanKhawar <ak78@njit.edu>
2023-05-30 19:50:08 -0400

    added a login form for username and password

---
+++ login.php
@@ -0,0 +1,17 @@
+<!--This page will give the user an input form to login to their account-->
+
+<?php
+    include_once 'header.php';
+?>
+<link rel="stylesheet" href="<?php echo 'styles.css' ?>">
+
+<!-- Creating login form -->
+<section class="login">
+    <h2>Login</h2>
+    <div class="login-form">
+        <form action="includes/login.inc.php" method="post">
+            <input type=text name=username placeholder="Username">
+            <input type=password name=password placeholder="Password">
+        </form>
+    </div>
+</section>

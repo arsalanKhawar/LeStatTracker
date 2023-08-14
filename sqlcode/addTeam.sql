b966dd6efc7835fb3d6d11be43a6f70fb5854ed9
unknown <sr1510@rutgers.edu>
2023-08-14 16:33:47 -0400

    addTeam SQL create code

--- sqlcode/addTeam.sql
+++ sqlcode/addTeam.sql
@@ -1,1 +1,6 @@
--- put team sql here (dont forget userid implementation)
+CREATE TABLE teams(
+    teamid int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    userid int NOT NULL,
+    teamname varchar(128) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users(userid)
+    );
